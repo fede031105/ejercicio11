3ca679c8f49cbfe637961559b71eec3cae30a589
Federico <fedelico431@gmail.com>
2026-05-08 23:15:30 -0600

    Initial commit

---
+++ src/main/java/com/upiiz/ejercicio11/Ejercicio11Application.java
@@ -0,0 +1,13 @@
+package com.upiiz.ejercicio11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicio11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/upiiz/ejercicio11/Ejercicio11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upiiz.ejercicio11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
